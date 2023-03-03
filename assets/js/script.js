1ff3f2ef2fbb6653277c8e01b408d1e52d8b6414
Tjcarroll1 <tjcarroll1@icloud.com>
2023-03-03 16:47:19 +1000

    added the sections for the site and started writing some functions to get the first question into its intended element

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+var questions = document.querySelector(".questions");
+
+function questionaire() {
+        wordBlank.textcontent = "First Question";
+        console.log("testing")
+        questionaire
+}
+
+questionaire
+
+
+
+
+
+/*
+// Start Button - going to need eventListener 'click' for both these steps
+    /// start button actions the timer
+
+    /// start button actions the questions
+
+// Answering a question triggers the next question
+
+// Incorrect answer triggers timer reduction
+
+// If statement to end the game
+    /// if timer runs out game ends
+    /// if final question is answered game ends
+*/
+
+
+
+
+/*            ***QUESTION FOR THE GAME***
+1. If you were to fly in a straight line, how many kilometers are between Sydney and London?
+        a) 22,000km
+        b) 8,000km
+        c) 17,000km (answer)
+        d) 30,000km
+
+2. How long is the world's shortest flight?
+        a) 5 minutes
+        b) 1 minute (answer - 2.7 km from Papa Westray to Orkney Westray - Islands of Scotland )
+        c) 3 minutes
+        d) 10 minutes
+
+3. Which is the busiest airport by passenger traffic?
+        a) Charles de Gualle (Paris)
+        b) Hethrow (London)
+        c) LAX (LA)
+        d) ATL (Atlanta) - Answer
+
+4. What is the most visited country in the world?
+        a) France (answer)
+        b) Italy
+        c) England
+        d) Unite States 
+*/
