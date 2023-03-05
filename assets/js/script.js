999f71ae3c9857b259d440abece5be1a9a29b550
Tjcarroll1 <tjcarroll1@icloud.com>
2023-03-05 16:02:25 +1000

    added more functions and re-wrote the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,180 @@
+// GOLBAL VAR
+var questionIndex = 0
+var win = 0
+var loss = 0
+var secondsLeft = 60
+var timerEl = document.querySelector(".timer")
+var timerMessageEl = document.getElementById('timerMessage')
 var questionContentEl = document.getElementById('questionContent');
 var answerContentEl = document.getElementById('answerContent');
 var rightAnswerContentEl = document.getElementById('rightAnswerContentEl');
 var startGameEl = document.getElementById('startGame');
 var nextQuestionEl = document.getElementById('nextQuestion');
-var questionnaireArray = [
-	questionnaire1,
-	//questionnaire2,
-	//questionnaire3,
-	//questionnaire4,
-	];
 var questionsAll =
 	[
 		{
 			question: "If you were to fly in a straight line, how many               kilometers are between Sydney and London?",
 			choices: ["A: 22,000km", "B: 8,000km", "C: 17,000km", "D: 30,000km",],
-			answer: 2
+			answer: "C: 17,000km"
 		},
 
 		{
 			question: "How long is the world's shortest flight",
 			choices: ["5 minutes", "1 minute", "3 minutes", "10 minutes",],
-			answer: 1
+			answer: "1 minute"
 		},
 
 		{
 			question: "Which is the busiest airport by passenger traffic",
 			choices: ["Charles de Gualle (Paris)", "Heathrow (London)", "LAX (Las Angeles)", "Hartsfield-Jackson (Atlanta)",],
-			answer: 3
+			answer: "Hartsfield-Jackson (Atlanta)"
 		},
 
 		{
 			question: "What is the most visited country in the world",
 			choices: ["France", "China", "Italy", "USA",],
-			answer: 0
+			answer: "France"
 		}
 	]
 
+
+
+
+
+
 // START THE GAME. Clicking the button calls the questionnaire1 function which 
-startGameEl.addEventListener('click', startGame)
+
+
+
 function startGame() {
-	questionnaire1()
+	console.log("We started the game")
+	questionnaire()
 }
 
-function questionnaire1() {
-	questionContentEl.innerHTML = `<div>${questionsAll[0].question}</div>
-	<button class ="answer0">${questionsAll[0].choices[0]}</button>
-	<button class ="answer0">${questionsAll[0].choices[1]}</button>
-	<button class ="answer0">${questionsAll[0].choices[2]}</button>	
-	<button class ="answer0">${questionsAll[0].choices[3]}</button>
+function checkAnswer () {
+if (this.textContent == questionsAll[questionIndex].answer){
+ console.log("ans is right");
+rightAnswerContentEl.textContent = "This just got added";
+}
+win++
+/* need to fix line below
+if(questionIndex !=0)
+questionnaire();
+}
+else{
+  console.log("wrong ans");
+loss++
+if(secondsLeft < 5){
+clearInterval(countDown)
+//stop quiz
+}
+timeleft -5
+questionnaire()
+
+}
+*/
+}
+
+
+
+function questionnaire() {
+	if (questionIndex == 0) {
+		countDown();
+		score = 0
+	}
+
+	questionContentEl.innerHTML = `<div>${questionsAll[questionIndex].question}</div>
+	<button class ="answer0">${questionsAll[questionIndex].choices[0]}</button>
+	<button class ="answer0">${questionsAll[questionIndex].choices[1]}</button>
+	<button class ="answer0">${questionsAll[questionIndex].choices[2]}</button>	
+	<button class ="answer0">${questionsAll[questionIndex].choices[3]}</button>
 	`;
 
 	document.querySelectorAll('.answer0').forEach(item => {
-	item.addEventListener('click', event => {})
+		item.addEventListener('click', checkAnswer)
 	})
-	if (".answer0" === 'C: 17,000km') {
-		console.log ("Yes!!!! You're off to a great start");
-	} else {
-		console.log("Shame! That wasn't it. The answer was C: 17,000km.");
-	}  
+
 }
 
-questionnaire1 ()
+//  TIMER FOR THE GAME STARTS AT 2 MINUTES 
+function countDown() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft + " seconds left till end of game.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+function sendMessage() {
+  timerMessageEl.textContent = "You were too slow! Your Plane crashed! ";
+}
+
+startGameEl.addEventListener('click', startGame)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//questionnaire1 ()
 
 /*
-//call right answer function	
-
-//document.getElementById('answer1')choices.addEventListener('click', answer)
-function answer() {
-if (questionsAll.choices[2] === true) {
-		console.log ("Yes!!!! You're off to a great start");
-	} else {
-		console.log("Shame! That wasn't it. The answer was C: 17,000km.");
-	} 
-	answer()
-}
-
-
-
-
-
-
-
-//Right Answer Function
-//if click event on 'right answer' else if click on wrong answer Imight need to be a seperate function, deduct timers
-
-
-pseudo code for answers for question1: anser is [2]
-if 
-	button is clicked on choices array [2], 
-	console log You're off to a great start
-else
-	Shame! That wasn't right. The correct answer was C - 17,000km
-
-
-
-
-
-
-// NEXT QUESTION - Event listener to click next question that calls the following function to continue playing the game
-
+// Click on Next and the following question comes up
 nextQuestionEl.addEventListener('click', nextQuestion)
 
 function nextQuestion() {
@@ -131,60 +207,4 @@ function questionnaire4() {
 	<button>${questionsAll[0].choices[3]}</button>
 	`;	
 }
-
-
-//  TIMER FOR THE GAME STARTS AT 2 MINUTES 
-var el = document.getElementById("timer"),
-	mins = 2;
-	secs = 0;
-
-function countDown() {
-	if (secs || mins) {
-		setTimeout(countDown, 1000);
-	}
-
-	el.innerHTML = mins + ":" + (secs.toString().length < 2 ? "0" + secs : secs); 
-	secs -= 1;
-	if (secs < 0) {
-		mins -= 1;
-		secs = 59;
-	}
-}
-countDown();
-
-
-/*
-
-// If the start button is pushed run function 1
-// if next but is pushed a function that runs through questionsAll array 1,2,3
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Start Button - going to need eventListener 'click' for both these steps
-		/// start button actions the timer
-
-		/// start button actions the questions
-
-// Answering a question triggers the next question
-
-// Incorrect answer triggers timer reduction
-
-// If statement to end the game
-		/// if timer runs out game ends
-		/// if final question is answered game ends
-
 */
-
